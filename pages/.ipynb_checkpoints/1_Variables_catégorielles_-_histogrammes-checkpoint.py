09b25d19f35acff5ab9b1bbd47dbd472f65a430d
louiseligonniere <louise.ligonniere@orange.fr>
2023-12-17 17:07:34 +0000

    Dév application - graphes interactifs
    Passage à des graphes interactifs (plotly) sur les 3 premières pages
    Développement d'onglets de navigation entre les variables
    Création d'un notebook visualisation qui reprend les codes utilisés pour les graphes

--- pages/.ipynb_checkpoints/1_Variables_catégorielles_-_histogrammes-checkpoint.py
+++ pages/.ipynb_checkpoints/1_Variables_catégorielles_-_histogrammes-checkpoint.py
@@ -4,7 +4,8 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
+import plotly
+import plotly.express as px
 
 st.set_page_config(
     page_title="Variables catégorielles : histogrammes", 
@@ -19,43 +20,109 @@ st.markdown(
     """
 )
 
+# Chargement des données
 df = pd.read_csv("stackoverflow_full.csv")
 
-# Configuration des paramètres de visualisation
-sns.set(style="whitegrid")
+# Recodage des variables catégorielles (ENG -> FR)
+df_fr = df.copy()
 
-# Tracé des distributions des colonnes démographiques clés et de leur relation avec le statut 'Employed'
-# Âge vs. Employé
-fig = plt.figure(figsize=(4, 5))
-sns.histplot(data = df, x="Age", hue="Employed", multiple = "dodge", shrink=.8)
-plt.title("Distribution de l'âge vs. Statut d'emploi")
-plt.xlabel("Âge", fontsize=10)
-plt.ylabel(" ")
-st.pyplot(fig.get_figure())
+df_fr["EmployedCat"] = pd.cut(df_fr["Employed"], bins=[-1, 0, 1], labels=["Sans emploi", "En emploi"]).astype("object")
 
-# Genre vs. Employé
-fig = plt.figure(figsize=(4, 5))
-sns.histplot(data=df, x="Gender", hue="Employed", multiple = "dodge", shrink=.8)
-plt.title("Distribution du genre vs. Statut d'emploi")
-plt.xlabel("Genre", fontsize=10)
-plt.ylabel(" ")
-st.pyplot(fig.get_figure())
+df_fr["Age"] = df_fr["Age"].replace(
+    ["<35", ">35"],
+    ["Moins de 35 ans", "Plus de 35 ans"]
+)
 
-# Niveau d'éducation (EdLevel) vs. Employé
-fig = plt.figure(figsize=(4, 5))
-sns.histplot(data=df, x="EdLevel", hue="Employed", multiple = "dodge", shrink=.8)
-plt.title("Niveau d'éducation vs. Statut d'emploi")
-plt.xlabel("Niveau d'éducation", fontsize=10)
-plt.ylabel(" ")
-plt.xticks(rotation=45)
-st.pyplot(fig.get_figure())
+df_fr["Accessibility"] = df_fr["Accessibility"].replace(
+    ["No", "Yes"],
+    ["Non", "Oui"]
+)
 
-# Branche principale (MainBranch) vs. Employé
-fig = plt.figure(figsize=(4, 5))
-sns.histplot(data=df, x="MainBranch", hue="Employed", multiple = "dodge", shrink=.8)
-plt.title("Branche professionnelle principale vs. Statut d'emploi")
-plt.xlabel("Branche professionnelle principale", fontsize=10)
-plt.ylabel(" ")
-st.pyplot(fig.get_figure())
+df_fr["EdLevel"] = df_fr["EdLevel"].replace(
+    ["NoHigherEd", "Undergraduate", "Master", "PhD", "Other"],
+    ["Pas d'éducation supérieure", "Licence", "Master", "Doctorat", "Autre"]
+)
 
+df_fr["Gender"] = df_fr["Gender"].replace(
+    ["Man", "Woman", "NonBinary"],
+    ["Homme", "Femme", "Non-Binaire"]
+)
 
+df_fr["MentalHealth"] = df_fr["MentalHealth"].replace(
+    ["No", "Yes"],
+    ["Non", "Oui"]
+)
+
+df_fr["MainBranch"] = df_fr["MainBranch"].replace(
+    ["Dev", "NotDev"],
+    ["Développement", "Autre"]
+)
+
+# 1. Graphe Age
+fig_age = px.histogram(df_fr, x="Age", color = "EmployedCat", barmode="group", text_auto = True)
+
+fig_age.update_layout(
+    title_text="Distribution du statut d'emploi selon l'âge",
+    xaxis_title_text='Age', 
+    yaxis_title_text="",
+    legend_title_text = "",
+    bargap=0.2, 
+    bargroupgap=0.1 
+)
+
+# 2. Graphe Genre
+fig_genre = px.histogram(df_fr, x="Gender", color = "EmployedCat", barmode="group", text_auto = True)
+
+fig_genre.update_layout(
+    title_text="Distribution du statut d'emploi selon le genre",
+    xaxis_title_text='Genre', 
+    yaxis_title_text="",
+    legend_title_text = "",
+    bargap=0.2, 
+    bargroupgap=0.1 
+)
+
+# 3. Graphe Niveau d'éducation
+fig_ed = px.histogram(df_fr, x="EdLevel", color = "EmployedCat", barmode="group", 
+                      text_auto = True, 
+                      category_orders = dict(EdLevel = ["Pas d'éducation supérieure", "Licence", "Master", "Doctorat", "Autre"])
+                     )
+
+fig_ed.update_layout(
+    title_text="Distribution du statut d'emploi selon le niveau d'éducation",
+    xaxis_title_text="Niveau d'éducation", 
+    yaxis_title_text="",
+    legend_title_text = "",
+    bargap=0.2, 
+    bargroupgap=0.1 
+)
+
+# 4. Graphe Branche pro
+fig_branch = px.histogram(df_fr, x="MainBranch", color = "EmployedCat", barmode="group", text_auto = True)
+
+fig_branch.update_layout(
+    title_text="Distribution du statut d'emploi selon la branche professionnelle",
+    xaxis_title_text="Branche professionnelle (principale)", 
+    yaxis_title_text="",
+    legend_title_text = "",
+    bargap=0.2, 
+    bargroupgap=0.1 
+)
+
+# Choix du graphe
+tab_age, tab_genre, tab_ed, tab_branch = st.tabs(["Age", "Genre", "Niveau d'éducation", "Branche professionnelle"])
+
+with tab_age:
+    st.plotly_chart(fig_age)
+with tab_genre:
+    st.plotly_chart(fig_genre)
+with tab_ed:
+    st.plotly_chart(fig_ed)
+with tab_branch:
+    st.plotly_chart(fig_branch)
+
+st.markdown(
+    """
+    On remarque donc que ...
+    """
+)
--- pages/3_Variables_numériques_-_boxplots.py
+++ pages/3_Variables_numériques_-_boxplots.py
@@ -4,9 +4,8 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
-from statsmodels.graphics.mosaicplot import mosaic
-from wordcloud import WordCloud
+import plotly
+import plotly.express as px
 
 st.set_page_config(
     page_title="Variables numériques : boxplots", 
@@ -21,38 +20,102 @@ st.markdown(
     """
 )
 
+# Chargement des données
 df = pd.read_csv("stackoverflow_full.csv")
 
-sns.set(style="whitegrid")
+# Recodage des variables catégorielles (ENG -> FR)
+df_fr = df.copy()
 
-# Emploi vs. salaire précédent
-fig = plt.figure(figsize=(4, 5))
-sns.boxplot(x="Employed", y="PreviousSalary", data=df)
-plt.title("Salaire précédent par statut d'emploi")
-plt.xlabel("Statut d'emploi", fontsize=10)
-plt.ylabel("Salaire précédent", fontsize=10)
-st.pyplot(fig.get_figure())
+df_fr["EmployedCat"] = pd.cut(df_fr["Employed"], bins=[-1, 0, 1], labels=["Sans emploi", "En emploi"]).astype("object")
 
-# Emploi vs. compétences en informatique
-fig = plt.figure(figsize=(4, 5))
-sns.boxplot(x="Employed", y="ComputerSkills", data=df)
-plt.title("Compétences en informatique par statut d'emploi")
-plt.xlabel("Statut d'emploi", fontsize=10)
-plt.ylabel("Compétences en informatique", fontsize=10)
-st.pyplot(fig.get_figure())
+df_fr["Age"] = df_fr["Age"].replace(
+    ["<35", ">35"],
+    ["Moins de 35 ans", "Plus de 35 ans"]
+)
 
-# Emploi vs. années de code pro
-fig = plt.figure(figsize=(4, 5))
-sns.boxplot(x="Employed", y="YearsCodePro", data=df)
-plt.title("Années de codage professionnel par statut d'emploi")
-plt.xlabel("Statut d'emploi", fontsize=10)
-plt.ylabel("Années de codage professionnel", fontsize=10)
-st.pyplot(fig.get_figure())
+df_fr["Accessibility"] = df_fr["Accessibility"].replace(
+    ["No", "Yes"],
+    ["Non", "Oui"]
+)
 
-# Emploi vs. années de code
-fig = plt.figure(figsize=(4, 5))
-sns.boxplot(x="Employed", y="YearsCode", data=df)
-plt.title("Années de codage par statut d'emploi")
-plt.xlabel("Statut d'emploi", fontsize=10)
-plt.ylabel("Années de codage", fontsize=10)
-st.pyplot(fig.get_figure())
+df_fr["EdLevel"] = df_fr["EdLevel"].replace(
+    ["NoHigherEd", "Undergraduate", "Master", "PhD", "Other"],
+    ["Pas d'éducation supérieure", "Licence", "Master", "Doctorat", "Autre"]
+)
+
+df_fr["Gender"] = df_fr["Gender"].replace(
+    ["Man", "Woman", "NonBinary"],
+    ["Homme", "Femme", "Non-Binaire"]
+)
+
+df_fr["MentalHealth"] = df_fr["MentalHealth"].replace(
+    ["No", "Yes"],
+    ["Non", "Oui"]
+)
+
+df_fr["MainBranch"] = df_fr["MainBranch"].replace(
+    ["Dev", "NotDev"],
+    ["Développement", "Autre"]
+)
+
+# Salaire précédent vs. statut d'emploi
+fig_salaire = px.box(df_fr, x="EmployedCat", y = "PreviousSalary")
+
+fig_salaire.update_layout(
+    title_text="Distribution du salaire précédent selon le statut d'emploi",
+    xaxis_title_text="Statut d'emploi", 
+    yaxis_title_text="Salaire précédent",
+    bargap=0.2, 
+    bargroupgap=0.1 
+)
+
+# Compétences en informatique vs. statut d'emploi
+fig_info = px.box(df_fr, x="EmployedCat", y = "ComputerSkills")
+
+fig_info.update_layout(
+    title_text="Distribution des compétences en informatique selon le statut d'emploi",
+    xaxis_title_text="Statut d'emploi", 
+    yaxis_title_text="Compétences en informatique",
+    bargap=0.2, 
+    bargroupgap=0.1 
+)
+
+# Années de code pro vs. statut d'emploi
+fig_codepro = px.box(df_fr, x="EmployedCat", y = "YearsCodePro")
+
+fig_codepro.update_layout(
+    title_text="Distribution des années de code professionnel selon le statut d'emploi",
+    xaxis_title_text="Statut d'emploi", 
+    yaxis_title_text="Années de code professionnel",
+    bargap=0.2, 
+    bargroupgap=0.1 
+)
+
+# Années de code vs. statut d'emploi
+fig_code = px.box(df_fr, x="EmployedCat", y = "YearsCode")
+
+fig_code.update_layout(
+    title_text="Distribution des années de code selon le statut d'emploi",
+    xaxis_title_text="Statut d'emploi", 
+    yaxis_title_text="Années de code",
+    bargap=0.2, 
+    bargroupgap=0.1 
+)
+
+# Choix du graphe
+tab_salaire, tab_info, tab_codepro, tab_code = st.tabs(["Salaire précédent", "Compétences en informatique", "Années de code professionnel", "Années de code"])
+
+with tab_salaire:
+    st.plotly_chart(fig_salaire)
+with tab_info:
+    st.plotly_chart(fig_info)
+with tab_codepro:
+    st.plotly_chart(fig_codepro)
+with tab_code:
+    st.plotly_chart(fig_code)
+
+st.markdown(
+    """
+    On remarque donc que ...
+    """
+)
