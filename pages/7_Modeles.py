86c64112411966d15f475f714a5f8dbe507935d8
Pierre-Clayton <125273957+Pierre-Clayton@users.noreply.github.com>
2023-12-29 07:35:12 +0000

    Maj modeles + appli

--- pages/7_Modeles.py
+++ pages/7_Modeles.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 from sklearn.linear_model import LinearRegression, LogisticRegression
-from joblib import load
+import joblib 
 
 # ==========================
 # Set up data
@@ -40,54 +40,25 @@ TO_DUMMIES = [
 ]
 
 # ==========================
-# Set up models from dumps
+# Set up models from dumps dx.Explainer
 # ==========================
 
-exp1 = dx.Explainer.load(open("explanations/explanations_dt_txt.txt", "rb"))
-exp2 = dx.Explainer.load(open("explanations/explanations_rf_b_txt.txt", "rb"))
-exp2_m = dx.Explainer.load(open("explanations/explanations_rf_txt.txt", "rb"))
-exp4 = dx.Explainer.load(open("explanations/explanations_xg_b_txt.txt", "rb"))
-exp4_m = dx.Explainer.load(open("explanations/explanations_xg_txt.txt", "rb"))
+X=df[['Age', 'Accessibility', 'EdLevel', 'Gender', 'MentalHealth', 'MainBranch','YearsCode', 'YearsCodePro', 'PreviousSalary', 'ComputerSkills']]
+y=df['Employed']
+
+exp1 = dx.Explainer(joblib.load("modeles/decision_tree_baseline.joblib"),X,y)
+exp1_m = dx.Explainer(joblib.load("modeles/decision_tree.joblib"),X,y)
+exp2 = dx.Explainer(joblib.load("modeles/random_forest_baseline.joblib"),X,y)
+exp2_m = dx.Explainer(joblib.load("modeles/random_forest.joblib"),X,y)
+exp3 = dx.Explainer(joblib.load("modeles/logistic_regression_baseline.joblib"),X,y)
+exp3_m = dx.Explainer(joblib.load("modeles/logistic_regression.joblib"),X,y)
+exp4 = dx.Explainer(joblib.load("modeles/xgboost_baseline.joblib"),X,y)
+exp4_m = dx.Explainer(joblib.load("modeles/xgboost.joblib"),X,y)
 
 
 # ==========================
 # Utils functions
 # ==========================
-def get_employed_bias_bar_figure(bias):
-    df_bias = df.groupby([bias, "Employed"]).size().reset_index(name="Number")
-    df_bias.Employed = (df_bias.Employed == 1)
-    fig = px.bar(
-        df_bias,
-        x=bias,
-        y="Number",
-        color="Employed",
-        title=f"Number of employed by : {bias}",
-        color_discrete_sequence=["grey", "darkred"],
-    )
-    return fig
-
-
-def get_employed_bias_pie_figure(bias):
-    df_bias = df.groupby([bias, "Employed"]).size().reset_index(name="Number")
-    fig_1 = px.pie(
-        df_bias, values="Number", names=bias, title="Analysis among the population"
-    )
-    df_bias = (
-        df[df.Employed == 1]
-        .groupby([bias, "Employed"])
-        .size()
-        .reset_index(name="Number")
-    )
-    fig_2 = px.pie(
-        df_bias, values="Number", names=bias, title="Analysis among the Employed"
-    )
-    return fig_1, fig_2
-
-
-def get_salary_bias_box_figure(bias):
-    fig = px.box(df, x=bias, y="PreviousSalary")
-    return fig
-
 
 def get_data_linear_regression(parameters, difference):
     val_cols = list(set(VAL_COLS).intersection(parameters).difference(difference))
@@ -148,17 +119,22 @@ def get_fairness_check(criteria, privileged):
     f_object_rf = exp2.model_fairness(
         protected=protected, privileged=privileged, label="Random Forest"
     )
+    f_object_lr = exp3.model_fairness(
+        protected=protected, privileged=privileged, label="Logistic Regression"
+    )
     f_object_gb = exp4.model_fairness(
         protected=protected, privileged=privileged, label="Gradient Boosting"
     )
-    return lambda t: f_object_dc.plot([f_object_rf, f_object_gb], type=t, show=False)
+    return lambda t: f_object_dc.plot([f_object_rf, f_object_lr, f_object_gb], type=t, show=False)
 
 
 def get_fairness_check_after_mitigation(criteria, privileged, model):
     protected = df[criteria]
     lookup = {
         "Random Forest": [exp2, exp2_m],
-        "Gradient Boosting": [exp4, exp4_m]
+        "Gradient Boosting": [exp4, exp4_m],
+        "Decision Tree": [exp1, exp1_m],
+        "Logistic Regression": [exp3, exp3_m]
     }
 
     f_object = lookup[model][0].model_fairness(
@@ -174,25 +150,11 @@ def get_fairness_check_after_mitigation(criteria, privileged, model):
 # User interface
 # ==========================
 
-with st.sidebar:
-    st.title("Ekimetrics Responsible AI")
-    st.image("LOGO-ENSAE.png")
-    st.subheader("Alyette")
-    st.subheader("Johanne")
-    st.subheader("Jacques")
-    st.subheader("Rémy")
-    st.subheader("Théo")
-    st.subheader("Tien-Thinh")
-
-(tab_analysis_employment,
- tab_analysis_salary,
- tab_linear_regression,
+(tab_linear_regression,
  tab_logistic_regression,
  tab_fairness_test,
  tab_bias_mitigation) = st.tabs(
     [
-        "Analysis: employment",
-        "Analysis: salary",
         "Linear Regression",
         "Logistic Regression",
         "Fairness Test",
@@ -201,75 +163,49 @@ with st.sidebar:
 )
 
 # ==========================
-# Analysis: employment
-# ==========================
-
-tab_analysis_employment.header("Data analysis by employment")
-criteria_selector_1 = tab_analysis_employment.selectbox(
-    "Which criteria to analyse with employment ?",
-    ["Age", "Gender", "Country", "MentalHealth", "Accessibility"],
-)
-
-tab_analysis_employment.plotly_chart(get_employed_bias_bar_figure(criteria_selector_1))
-
-figure_1, figure_2 = get_employed_bias_pie_figure(criteria_selector_1)
-tab_analysis_employment.plotly_chart(figure_1)
-tab_analysis_employment.plotly_chart(figure_2)
-
-# ==========================
-# Analysis : salary
-# ==========================
-
-tab_analysis_salary.header("Data analysis by Salary")
-criteria_selection_2 = tab_analysis_salary.selectbox(
-    "Which criteria to analyse with salary ?",
-    ["Age", "Gender", "Country", "MentalHealth", "Accessibility"],
-)
-
-figure_3 = get_salary_bias_box_figure(criteria_selection_2)
-tab_analysis_salary.plotly_chart(figure_3)
-
-# ==========================
 # Linear Regression
 # ==========================
 
-tab_linear_regression.header("Linear Regression")
+tab_linear_regression.header("Regression Linéaire")
 list_col3 = tab_linear_regression.multiselect(
-    "Select variable for Linear Regress: ",
+    "Sélectionner les variables pour la regression linéaire: ",
     VAL_COLS + TO_DUMMIES + ["Employed"],
     default=VAL_COLS[1:] + TO_DUMMIES + ["Employed"],
 )
 
 y_col3 = tab_linear_regression.selectbox("Select", VAL_COLS)
 result_df3, score3 = get_data_linear_regression(list_col3, y_col3)
-tab_linear_regression.subheader(f"The score is: {round(score3 * 100, 2)}%")
+tab_linear_regression.subheader(f"Le R2 du modèle est : {round(score3 * 100, 2)}%")
 tab_linear_regression.table(result_df3)
 
 # ==========================
 # Logistic Regression
 # ==========================
 
-tab_logistic_regression.header("Logistic Regression")
+tab_logistic_regression.header("Regression Logistique")
 list_col = tab_logistic_regression.multiselect(
-    "Select variable for Logistic Regress: ",
+    "Sélectionner les variables pour la regression logistique: ",
     VAL_COLS + TO_DUMMIES,
     default=VAL_COLS[1:] + TO_DUMMIES,
 )
 result_df, score, X, delta_prob = get_data_log_regression(parameters=list_col)
-tab_logistic_regression.subheader(f"The score is: {round(score * 100, 2)}%")
+tab_logistic_regression.subheader(f"Le R2 du modèle est : {round(score * 100, 2)}%")
 tab_logistic_regression.table(result_df)
 
 # ==========================
 # Fairness test
 # ==========================
 
-tab_fairness_test.header("Models on biased dataset performance:")
+tab_fairness_test.header("Modèles baseline:")
 
 tab_fairness_test.subheader("Decision Tree performance")
 tab_fairness_test.table(exp1.model_performance().result)
 
 tab_fairness_test.subheader("Random Forest performance")
 tab_fairness_test.table(exp2.model_performance().result)
+
+tab_fairness_test.subheader("Logistic Regression performance")
+tab_fairness_test.table(exp3.model_performance().result)
 
 tab_fairness_test.subheader("Gradient Boosting performance")
 tab_fairness_test.table(exp4.model_performance().result)
@@ -333,7 +269,7 @@ tab_bias_mitigation.header("Bias mitigation with Dalex")
 
 model_selector = tab_bias_mitigation.selectbox(
     "Which model should have its biases mitigated ?",
-    ["Random Forest", "Gradient Boosting"],
+    ["Random Forest", "Gradient Boosting", "Logistic Regression", "Decision Tree"],
     key="bias6_model_selectbox",
 )
 
